e8e5f8c32f99b123c939ebea54ee610597b37be2
SimonSun <yans3@illinois.edu>
2018-09-12 18:52:24 -0500

    "work 4/5 times"

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -33,14 +33,99 @@ public class SolveMaze {
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
          */
-        for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
-        }
-
-        if (maze.isFinished()) {
-            System.out.println("You solved the maze!");
+        //System.out.println(maze);
+        //for(int i = 100; i<100; i++) {
+            A: for (int step = 0; step < 1000; step++) {
+                System.out.println(step);
+                maze = isSolution(maze);
+                if (maze.isFinished()) {
+                    break A;
+                }
+            }
+            if (maze.isFinished()) {
+                System.out.println("You solved the maze!");
+            } else {
+                System.out.println("Try again!");
+            }
+        //}
+    }
+    public static Maze isSolution(final Maze maze){
+        int i = (int) (Math.random() * 20.0);
+        Maze m = maze;
+        if(i % 2 == 0) {
+            if (tryMoveLeft(m)) {
+                m.move();
+                //printStep(1);
+            } else if (tryMoveRight(m)) {
+                m.move();
+                //printStep(2);
+            } else if (m.canMove()) {
+                m.move();
+                //printStep(0);
+            } else {
+                m.turnLeft();
+                m.turnLeft();
+                m.move();
+                //printStep(3);
+            }
         } else {
-            System.out.println("Try again!");
+            if (tryMoveRight(m)) {
+                m.move();
+                //printStep(2);
+            } else if (tryMoveLeft(m)){
+                m.move();
+                //printStep(1);
+            } else if (m.canMove()) {
+                m.move();
+                //printStep(0);
+            } else {
+                m.turnLeft();
+                m.turnLeft();
+                m.move();
+                //printStep(3);
+            }
+        }
+        //System.out.println(i);
+        return m;
+    }
+    public static boolean tryMoveLeft(Maze maze){
+        boolean result = false;
+        maze.turnLeft();
+        if (maze.canMove()) {
+            result = true;
+        } else {
+            maze.turnRight();
+        }
+        System.out.println(result);
+        return result;
+    }
+    public static boolean tryMoveRight(Maze maze){
+        boolean result = false;
+        maze.turnRight();
+        if (maze.canMove()) {
+            result = true;
+        } else {
+            maze.turnLeft();
+        }
+        System.out.println(result);
+        return result;
+    }
+    public static void printStep(int s){
+        String dir = "";
+        switch(s){
+            case 0:
+                dir = "foward";
+                break;
+            case 1:
+                dir = "left";
+                break;
+            case 2:
+                dir = "right";
+                break;
+            case 3:
+                dir = "turn backward";
+                break;
         }
     }
+
 }
